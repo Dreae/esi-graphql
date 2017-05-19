313d14c49e063d1741a31ec97588a29049f5fa6c
Dreae <dreae@dreae.onl>
2017-05-18 23:57:53 -0400

    Add authorization support

--- main.go
+++ main.go
@@ -1,20 +1,22 @@
 package main
 
 import (
+	"encoding/json"
 	"io/ioutil"
 	"log"
 	"net/http"
 
+	"context"
+
 	"github.com/dreae/esi-graphql/resolvers"
 	"github.com/neelance/graphql-go"
-	"github.com/neelance/graphql-go/relay"
 )
 
 var schema *graphql.Schema
 
 func init() {
 	var err error
-	schemaFile, err := ioutil.ReadFile("schema.gql")
+	schemaFile, err := ioutil.ReadFile("./assets/schema.gql")
 	if err != nil {
 		panic(err)
 	}
@@ -26,52 +28,38 @@ func init() {
 }
 
 func main() {
+	page, err := ioutil.ReadFile("./assets/index.html")
+	if err != nil {
+		panic("Could not read index file from ./assets/index.html")
+	}
+
 	http.Handle("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Write(page)
 	}))
 
-	http.Handle("/query", &relay.Handler{Schema: schema})
+	http.Handle("/query", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var params struct {
+			Query         string                 `json:"query"`
+			OperationName string                 `json:"operationName"`
+			Variables     map[string]interface{} `json:"variables"`
+		}
+		if err := json.NewDecoder(r.Body).Decode(&params); err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		ctx := context.WithValue(r.Context(), "auth", r.Header.Get("Authorization"))
+
+		response := schema.Exec(ctx, params.Query, params.OperationName, params.Variables)
+
+		responseJSON, err := json.Marshal(response)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.Write(responseJSON)
+	}))
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
-
-var page = []byte(`
-<!DOCTYPE html>
-<html>
-	<head>
-		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/graphiql/0.7.8/graphiql.css" />
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/fetch/1.0.0/fetch.min.js"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/react/15.3.2/react.min.js"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/react/15.3.2/react-dom.min.js"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/graphiql/0.7.8/graphiql.js"></script>
-	</head>
-	<body style="width: 100%; height: 100%; margin: 0; overflow: hidden;">
-		<div id="graphiql" style="height: 100vh;">Loading...</div>
-		<script>
-			function graphQLFetcher(graphQLParams) {
-				graphQLParams.variables = graphQLParams.variables ? JSON.parse(graphQLParams.variables) : null;
-				return fetch("/query", {
-					method: "post",
-					body: JSON.stringify(graphQLParams),
-					credentials: "include",
-					headers: {
-						"X-Foo-Bar": "Baz"
-					},
-				}).then(function (response) {
-					return response.text();
-				}).then(function (responseBody) {
-					try {
-						return JSON.parse(responseBody);
-					} catch (error) {
-						return responseBody;
-					}
-				});
-			}
-			ReactDOM.render(
-				React.createElement(GraphiQL, {fetcher: graphQLFetcher}),
-				document.getElementById("graphiql")
-			);
-		</script>
-	</body>
-</html>
-`)
