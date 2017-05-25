40210aca757b61ee0f8823cdc2373a6120e59e74
Dreae <dreae@dreae.onl>
2017-05-25 19:52:57 -0400

    Switch to memcached
    Possible bug in httpcache redis implementation?

--- resolvers/http/init.go
+++ resolvers/http/init.go
@@ -3,27 +3,14 @@ package http
 import (
 	"os"
 
-	"github.com/garyburd/redigo/redis"
 	"github.com/gregjones/httpcache"
-	httpredis "github.com/gregjones/httpcache/redis"
+	"github.com/gregjones/httpcache/memcache"
 )
 
 // InitHTTP initializes the HTTP client using an appropriate cache service
 func InitHTTP() {
-	if redisURL := os.Getenv("REDIS_HOST"); redisURL != "" {
-
-		redisPass := os.Getenv("REDIS_PASS")
-		var options []redis.DialOption
-		if redisPass != "" {
-			options = append(options, redis.DialPassword(redisPass))
-		}
-
-		conn, err := redis.Dial("tcp", redisURL, options...)
-		if err != nil {
-			panic(err)
-		}
-
-		client = httpcache.NewTransport(httpredis.NewWithClient(conn)).Client()
+	if memcachedURL := os.Getenv("MEMCACHE_URL"); memcachedURL != "" {
+		client = httpcache.NewTransport(memcache.New(memcachedURL)).Client()
 	} else {
 		client = httpcache.NewTransport(httpcache.NewMemoryCache()).Client()
 	}
